dab36e9ac8ce40551e9c24df576af8b6c2139066
justinkuntz <justin@creativesoapbox.com>
2025-04-29 19:32:04 -0700

    Style blog overview

--- src/components/Blog.tsx
+++ src/components/Blog.tsx
@@ -33,8 +33,9 @@ export default function Blog({ data, tags }: Props) {
 
   return (
     <div class="grid grid-cols-1 gap-6">
-      <div class="col-span-3 sm:col-span-1">
-        <div class="sticky top-24">
+      <div class="col-span-3 mt-12 md:mt-24">
+        <div class="flex items-center justify-end gap-3">
+        <div class="text-sm font-semibold uppercase text-surface10">Filter:</div>
           <ul class="flex flex-wrap gap-1.5">
             <For each={tags}>
               {(tag) => (
@@ -52,12 +53,9 @@ export default function Blog({ data, tags }: Props) {
           </ul>
         </div>
       </div>
-      <div class="col-span-3 sm:col-span-2">
+      <div class="col-span-3">
         <div class="flex flex-col">
-          <div class="text-sm uppercase mb-2">
-            SHOWING {posts().length} OF {data.length} POSTS
-          </div>
-          <ul class="flex flex-col gap-3">
+          <ul class="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
             {posts().map((post) => (
               <li>
                 <ArrowCard entry={post} />
