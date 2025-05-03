793f71f5002c49f6364889938f6d4007c72143e6
justinkuntz <justin@creativesoapbox.com>
2025-05-02 22:33:36 -0700

    Swap to NPM and style remaining portfolio pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,17 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-
+import rehypeUnwrapImages from "rehype-unwrap-images";
 import solidJs from "@astrojs/solid-js";
 
 import fuse from "astro-fuse";
 
 export default defineConfig({
   site: "https://astro-nano-demo.vercel.app",
-  integrations: [mdx(), sitemap(), tailwind(), solidJs(), fuse()],
+  integrations: [
+    mdx(), sitemap(), tailwind(), solidJs(), fuse()
+  ],
+  markdown: {
+    remarkPlugins: [rehypeUnwrapImages]
+  }
 });
